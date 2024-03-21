2d46a87a5b7b40024b1528d7fa4e5661ef5856d9
Drew <dch528@gmail.com>
2024-03-21 16:36:54 -0400

    Drew Hairston Organizing Bears Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 15, 'M', 'Brown', 'Aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M', 'Black', 'Obviously rowdy', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'F', 'Brown', 'Bully', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 100, 'Male', 'Brown', 'Stalwart', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 4, 'F', 'Black', 'Playful', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 66, 'M', 'Brown', 'Awkward', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 45, 'F', 'Brown', 'Shrewd', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 666, 'M', 'Brown', 'Based', true);
